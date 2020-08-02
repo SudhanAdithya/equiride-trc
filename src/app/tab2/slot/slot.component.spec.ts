ce187ca086c01543404b15b5e220b0afed3ef5d1
sudhan <suthan@Sudhans-MacBook-Air-1598.local>
2020-08-02 17:44:45 +0530

    booking routes

---
+++ src/app/tab2/slot/slot.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { SlotComponent } from './slot.component';
+
+describe('SlotComponent', () => {
+  let component: SlotComponent;
+  let fixture: ComponentFixture<SlotComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SlotComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SlotComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
